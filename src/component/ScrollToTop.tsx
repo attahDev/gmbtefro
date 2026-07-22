5360d94a88b03ef0f859c10b539a4f0cf45c07b3
attahDev <attahumana007@gmail.com>
2026-07-22 11:08:54 +0100

    Update ScrollToTop.tsx

--- src/component/ScrollToTop.tsx
+++ src/component/ScrollToTop.tsx
@@ -1,1 +1,20 @@
 
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to the top whenever the route changes
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "auto",
+    });
+  }, [pathname]);
+
+  return null;
+};
+
+export default ScrollToTop;
